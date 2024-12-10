fbf3f3da58f710d5614f5fc4f666a4a0363ad3e9
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-10 23:10:49 +0300

    RouterDom

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+import Search from './pages/Search'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="videos/:id" element={<Detail />} />
+        <Route path="search/:keyword" element={<Search />} />
+      </Routes>
+    </BrowserRouter >
   )
 }
+
 
 export default App
