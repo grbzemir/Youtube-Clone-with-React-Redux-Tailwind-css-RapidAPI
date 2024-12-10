ad9670fd11a9c969b040063066d5499b86d1b7d1
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-10 23:25:50 +0300

    images

---
+++ Youtube/src/assetss/images/images.jsx
@@ -0,0 +1,10 @@
+import youtube from './youtube.png';
+import deneme from './youtube1.png';
+
+
+export default {
+
+    youtube: youtube,
+    deneme: deneme,
+
+}
--- Youtube/src/components/Navbar.jsx
+++ Youtube/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="w-full flex items-center justify-between h-20 px-3">
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
