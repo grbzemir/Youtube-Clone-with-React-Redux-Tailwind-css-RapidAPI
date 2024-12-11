6f3fba64761d5d705dc48bbf0f5748da9e849650
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-12 00:03:04 +0300

    .

--- Youtube/src/pages/Search.jsx
+++ Youtube/src/pages/Search.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import SearchCard from '../components/SearchCard'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import searchAction from '../redux/actions/SearchData'
+
 
 
 const Search = () => {
